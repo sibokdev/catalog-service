ec783e03965862ab5ad5d3c992f75068de46d6a1
Alfredo Suárez Romero <48132178+sibokdev@users.noreply.github.com>
2023-10-26 13:52:08 -0600

    first commit

---
+++ src/main/java/com/polarbookshop/catalogservice/services/BookService.java
@@ -0,0 +1,44 @@
+package com.polarbookshop.catalogservice.services;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookAlreadyExistsException;
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public Iterable<Book> viewBookList() {
+        return bookRepository.findAll();
+    }
+    public Book viewBookDetails(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+ .orElseThrow(() -> new BookNotFoundException(isbn));
+    }
+    public Book addBookToCatalog(Book book) {
+        if (bookRepository.existsByIsbn(book.isbn())) {
+            throw new BookAlreadyExistsException(book.isbn());
+        }
+        return bookRepository.save(book);
+    }
+    public void removeBookFromCatalog(String isbn) {
+        bookRepository.deleteByIsbn(isbn);
+    }
+    public Book editBookDetails(String isbn, Book book) {
+        return bookRepository.findByIsbn(isbn)
+                .map(existingBook -> {
+                    var bookToUpdate = new Book(
+                            existingBook.isbn(),
+                            book.title(),
+                            book.author(),
+                            book.price());
+                    return bookRepository.save(bookToUpdate);
+                })
+                .orElseGet(() -> addBookToCatalog(book));
+    }
+
+}
