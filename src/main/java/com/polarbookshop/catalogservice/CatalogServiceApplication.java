c6ef19bb7be5ab0548ce0d71c09171b1dd532b47
Alfredo Suárez Romero <48132178+sibokdev@users.noreply.github.com>
2024-01-31 13:12:43 -0600

    Begining env configs

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,10 +2,10 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
+@ConfigurationPropertiesScan // enabling scanning of config data beans
 public class CatalogServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
---
+++ src/main/java/com/polarbookshop/config/PolarProperties.java
@@ -0,0 +1,18 @@
+package com.polarbookshop.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+//class to define custom properties in a spring bean
+@ConfigurationProperties(prefix = "polar") // marks the class as config with the prefix polar
+public class PolarProperties {
+    /**
+     * A message to welcome users.
+     */
+    private String greeting; // propertie can be accesed with prefix + name example polar.greeting
+    public String getGreeting() {
+        return greeting;
+    }
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
