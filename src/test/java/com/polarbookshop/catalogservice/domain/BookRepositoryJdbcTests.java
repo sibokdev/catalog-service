efd3641a0b9af220ac6d693fac42fcdeb4bab3ee
Alfredo Suárez Romero <48132178+sibokdev@users.noreply.github.com>
2024-02-07 12:45:24 -0600

    Creating more jdbc test and setting integration-profile for full integration tests

--- src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -1,6 +1,8 @@
 package com.polarbookshop.catalogservice.domain;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import com.polarbookshop.catalogservice.config.DataConfig;
 import com.polarbookshop.catalogservice.repository.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJdbcTest //Identifies a test class that focuses on Spring Data JDBC components
 @Import(DataConfig.class) //Imports the data configuration (needed to enable auditing)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Disables the default behavior of relying on an embedded test database since we want to use Testcontainers
@@ -20,6 +23,21 @@ class BookRepositoryJdbcTests {
     private BookRepository bookRepository;
     @Autowired
     private JdbcAggregateTemplate jdbcAggregateTemplate; //a lower-level object we can use to set up the context for each test case instead of using the repository (the object under testing).
+
+    @Test
+    void findAllBooks() {
+        var book1 = Book.of("1234561235", "Title", "Author", 12.90);
+        var book2 = Book.of("1234561236", "Another Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(book1);
+        jdbcAggregateTemplate.insert(book2);
+
+        Iterable<Book> actualBooks = bookRepository.findAll();
+
+        assertThat(StreamSupport.stream(actualBooks.spliterator(), true)
+                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2.isbn()))
+                .collect(Collectors.toList())).hasSize(2);
+    }
+
     @Test
     void findBookByIsbnWhenExisting() {
         var bookIsbn = "1234561237";
@@ -28,5 +46,39 @@ class BookRepositoryJdbcTests {
         Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
         assertThat(actualBook).isPresent();
         assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+
+    @Test
+    void findBookByIsbnWhenNotExisting() {
+        Optional<Book> actualBook = bookRepository.findByIsbn("1234561238");
+        assertThat(actualBook).isEmpty();
+    }
+
+    @Test
+    void existsByIsbnWhenExisting() {
+        var bookIsbn = "1234561239";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(bookToCreate);
+
+        boolean existing = bookRepository.existsByIsbn(bookIsbn);
+
+        assertThat(existing).isTrue();
+    }
+
+    @Test
+    void existsByIsbnWhenNotExisting() {
+        boolean existing = bookRepository.existsByIsbn("1234561240");
+        assertThat(existing).isFalse();
+    }
+
+    @Test
+    void deleteByIsbn() {
+        var bookIsbn = "1234561241";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 12.90);
+        var persistedBook = jdbcAggregateTemplate.insert(bookToCreate);
+
+        bookRepository.deleteByIsbn(bookIsbn);
+
+        assertThat(jdbcAggregateTemplate.findById(persistedBook.id(), Book.class)).isNull();
     }
 }
